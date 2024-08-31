d990cf774dc65aa0498ca34d98030a81fe34a581
Merethe Granevang <fabeline@hotmail.com>
2024-08-31 18:23:00 +0200

    Added minimap button

--- MountSelector.lua
+++ MountSelector.lua
@@ -42,3 +42,56 @@ end
 
 -- Create the buttons when the addon is loaded
 createMountButtons()
+
+-- Create the Minimap Button
+local mountSelectorMinimapButton = CreateFrame("Button", "MountSelectorMinimapButton", Minimap)
+mountSelectorMinimapButton:SetSize(32, 32) -- Size of the minimap button
+mountSelectorMinimapButton:SetFrameStrata("MEDIUM")
+mountSelectorMinimapButton:SetFrameLevel(8)
+
+-- Set the minimap button's texture
+mountSelectorMinimapButton:SetNormalTexture("Interface\\AddOns\\MountSelector\\Textures\\MountSelector") -- Path to the normal texture
+mountSelectorMinimapButton:SetPushedTexture("Interface\\AddOns\\MountSelector\\Textures\\MountSelector") -- Path to the pushed texture
+mountSelectorMinimapButton:SetHighlightTexture("Interface\\Minimap\\UI-Minimap-ZoomButton-Highlight") -- Highlight texture
+
+-- Position the minimap button
+mountSelectorMinimapButton:SetPoint("TOPLEFT", Minimap, "TOPLEFT", -10, -10)
+
+-- Make the button draggable around the minimap
+mountSelectorMinimapButton:RegisterForDrag("LeftButton")
+mountSelectorMinimapButton:SetScript("OnDragStart", function(self)
+    self:StartMoving()
+end)
+mountSelectorMinimapButton:SetScript("OnDragStop", function(self)
+    self:StopMovingOrSizing()
+end)
+
+-- Set up the click event to toggle the MountSelector window
+mountSelectorMinimapButton:SetScript("OnClick", function(self, button)
+    if mountSelectorFrame:IsShown() then
+        mountSelectorFrame:Hide()
+    else
+        mountSelectorFrame:Show()
+    end
+end)
+
+-- Set the minimap button texture (you may need to adjust the file paths)
+if not mountSelectorMinimapButton:GetNormalTexture() then
+    local tex = mountSelectorMinimapButton:CreateTexture(nil, "BACKGROUND")
+    tex:SetTexture("Interface\\AddOns\\MountSelector\\Textures\\MountIconNormal")
+    tex:SetAllPoints()
+    mountSelectorMinimapButton:SetNormalTexture(tex)
+end
+
+-- Initialize the MountSelector frame as hidden
+mountSelectorFrame:Hide()
+
+
+-- Function to update the button's position around the minimap
+local function updateMinimapButtonPosition()
+    local angle = math.rad(45) -- Adjust this value to place the button at a different angle
+    local x = cos(angle) * 80
+    local y = sin(angle) * 80
+    mountSelectorMinimapButton:SetPoint("CENTER", Minimap, "CENTER", x, y)
+end
+
