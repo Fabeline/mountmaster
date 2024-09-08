c64938427b4f5e9a120ffd9e6ee17197de7e68a7
Merethe Granevang <fabeline@hotmail.com>
2024-09-08 18:30:16 +0200

    Added better toggle for types

--- MountFilterDropdown.lua
+++ MountFilterDropdown.lua
@@ -26,8 +26,55 @@ function getAllColors()
     return colors
 end
 
+function getAllTypes()
+    -- Collect all unique skeleton types from the mounts data
+    local typeSet = {}
+    for _, mount in ipairs(mounts) do
+        if mount.skeleton_type and mount.skeleton_type ~= "" then
+            local lowerType = string.lower(mount.skeleton_type)
+            typeSet[lowerType] = mount.skeleton_type
+        end
+    end
+
+    -- Convert the set of skeleton types to a list and sort
+    local types = {}
+    for _, originalType in pairs(typeSet) do
+        table.insert(types, CapitalizeFirstLetter(originalType))
+    end
+
+    table.sort(types)
+    return types
+end
+
 local function allColorsAreSelected()
     return #selectedColors == #getAllColors()
+end
+
+local function allTypesAreSelected()
+    return #selectedTypes == #getAllTypes()
+end
+
+local function toggleType(type)
+    if(type == "All") then
+        if allTypesAreSelected() then
+            selectedTypes = {}
+        else
+            selectedTypes = getAllTypes()
+        end
+    else 
+        -- add type to selectedTypes
+        if not tContains(selectedTypes, type) then
+            table.insert(selectedTypes, type)
+        else -- remove type from selectedTypes
+            for i, v in ipairs(selectedTypes) do
+                if v == type then
+                    table.remove(selectedTypes, i)
+                end
+            end
+        end
+    end
+    saveSelectedTypes(selectedTypes)
+    renderMounts()
 end
 
 local function toggleColor(color)
@@ -57,28 +104,6 @@ local function toggleColor(color)
     renderMounts()
 end
 
-local function toggleType(type)
-    if(type == "All") then
-        selectedTypes = {}
-    else 
-        -- add type to selectedTypes
-        if not tContains(selectedTypes, type) then
-            table.insert(selectedTypes, type)
-        else -- remove type from selectedTypes
-            for i, v in ipairs(selectedTypes) do
-                if v == type then
-                    table.remove(selectedTypes, i)
-                end
-            end
-        end
-
-        --for i, v in ipairs(selectedTypes) do
-        --    print(v)
-        --end
-    end
-    saveSelectedTypes(selectedTypes)
-    renderMounts()
-end
 
 -- Populate the dropdown menu with skeleton types
 local function initializeSkeletonDropdown(self, level)
@@ -92,29 +117,11 @@ local function initializeSkeletonDropdown(self, level)
     -- Add "All" option
     info.text = "All"
     info.value = "All"
-    info.checked = (#selectedTypes == 0)
+    info.checked = allTypesAreSelected()
     UIDropDownMenu_AddButton(info, level)
 
-    -- Collect all unique skeleton types from the mounts data, case-insensitively
-    local skeletonSet = {}
-    for _, mount in ipairs(mounts) do
-        if mount.skeleton_type and mount.skeleton_type ~= "" then
-            local lowerSkeleton = string.lower(mount.skeleton_type)
-            skeletonSet[lowerSkeleton] = mount.skeleton_type  -- Store the original case
-        end
-    end
-
-    -- Convert the set of skeleton types to a list
-    local skeletons = {}
-    for _, originalSkeleton in pairs(skeletonSet) do
-        table.insert(skeletons, CapitalizeFirstLetter(originalSkeleton))
-    end
-
-    -- Sort the skeleton types alphabetically
-    table.sort(skeletons)
-
     -- Add each unique skeleton type to the dropdown
-    for _, skeleton in ipairs(skeletons) do 
+    for _, skeleton in ipairs(getAllTypes()) do 
         info.text, info.value = skeleton, skeleton
         info.checked = tContains(selectedTypes, skeleton)
         UIDropDownMenu_AddButton(info, level)
@@ -135,10 +142,6 @@ local function initializeColorDropdown(self, level)
     info.value = "All"
     info.checked = (allColorsAreSelected())
     UIDropDownMenu_AddButton(info, level)
-
-    if #selectedColors == 0 then
-        info.checked = true
-    end
 
     -- Add each unique color to the dropdown
     for _, color in ipairs(getAllColors()) do  -- Use ipairs to correctly iterate over the sorted list
@@ -163,7 +166,7 @@ function filterMounts(visibleMounts)
 
     for i = 1, #visibleMounts do
         local currentMount = visibleMounts[i]
-        if (tContains(selectedColors, CapitalizeFirstLetter(currentMount.color))) and (#selectedTypes == 0 or  tContains(selectedTypes, CapitalizeFirstLetter(currentMount.skeleton_type))) then
+        if (tContains(selectedColors, CapitalizeFirstLetter(currentMount.color))) and tContains(selectedTypes, CapitalizeFirstLetter(currentMount.skeleton_type)) then
             table.insert(filteredMounts, currentMount)
         end
 
